8199803f1e91f9637a11ac5d3ea466c85faf646d
0b1Kn00b <polysemantic@gmail.com>
2012-02-16 15:29:44 +0100

    Added error folder

---
+++ src/main/haxe/stax/error/AbstractMethodError.hx
@@ -0,0 +1,15 @@
+package stax.error;
+import haxe.PosInfos;
+
+/**
+ * ...
+ * @author 0b1kn00b
+ */
+
+class AbstractMethodError extends Error {
+	
+	public function new(?pos:PosInfos) {
+		super( "Called abstract method at " + pos );
+	}
+	
+}
