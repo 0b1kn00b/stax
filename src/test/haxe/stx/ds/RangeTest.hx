ae2427720ccc7857c5bdba83a79badbfa0602e13
0b1Kn00b <polysemantic@gmail.com>
2012-06-05 17:23:06 +0200

    added Range

---
+++ src/test/haxe/stx/ds/RangeTest.hx
@@ -0,0 +1,56 @@
+﻿package stx.ds;
+import stx.ds.Range;
+using stx.Functions;
+using stx.Dynamics;
+
+class RangeTest extends stx.test.TestCase{
+	
+	public function testDefaultInitialisation(){
+		var range = Range.create();
+		assertTrue(range.min == -1.7976931348623157 * Math.pow(10,308));
+		assertTrue(range.max == 1.7976931348623157 * Math.pow(10,308));
+	}
+	public function testRange(){
+		var a = Range.create();
+		var range = Range.create();
+
+		assertTrue(a.equals(range));
+	}
+	/*public function testClone(){
+		var range = Range.create(-10,10);
+		var a = range.clone();
+		assertTrue(a.equals(range));
+	}*/
+	public function testOverlap(){
+		var a 		= Range.create(10,110);
+		var b 		= Range.create(180,260);
+		var range = Range.create(100,200);
+
+		assertTrue(range.overlap(a));
+		assertTrue(range.overlap(b));
+		assertFalse(a.overlap(b));
+	}
+	public function testWithin(){
+		var range = Range.create(-10,10);
+		assertTrue(range.within(0));
+		assertFalse(range.within(-11));
+		assertFalse(range.within(10.00001));
+	}
+	public function testInside(){
+		var a = Range.create(-100,-10);
+		var range = Range.create(-90,-20);
+
+		assertTrue(range.inside(a));
+		range = 
+			range.into(
+				function(x){
+					return Range.create(-110,x.max);
+				}
+			);
+		assertFalse(range.inside(a));
+		range = 
+			range.into( Range.create.lazy(-90,0).promote() );
+
+		assertFalse(range.inside(a));
+	}
+}
