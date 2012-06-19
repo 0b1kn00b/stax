6f4b6732964b1d94b17640156e75292d1b25bf67
0b1Kn00b <polysemantic@gmail.com>
2012-06-19 18:32:11 +0200

    Added haxelib install routine (rudimentary)

---
+++ tools/Run.hx
@@ -0,0 +1,28 @@
+import stx.Prelude;
+import stx.Tuples;
+import haxe.io.Input;
+import stx.reactive.Arrows; 	using stx.reactive.Arrows;
+															using stx.Eithers;
+															using stx.Strings;
+class Run{
+
+	static function main(){
+		ProcessArrow.process.trace()
+			.then(
+				function(x:Input){
+					return x.readLine().toString().append('stax/git/src/main/haxe');
+				}.lift()
+				.then(
+					function(x:String):Array<String>{
+						return ['haxelib','dev','stax',x];
+					}.lift()
+				).then( ProcessArrow.process )
+			 	.then(
+			 			function(x:Tuple2<Input,Input>):Tuple2<Input,Input>{
+			 				trace(x._2.readLine());
+			 				return x;
+			 			}.lift()
+			 	).trace().second()
+		).run(['haxelib','config']);
+	}
+}
