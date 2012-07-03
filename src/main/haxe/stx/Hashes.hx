71eb25fd23ff79f88cfe16c0e3a4b9a19c05b725
0b1Kn00b <polysemantic@gmail.com>
2012-07-03 21:34:15 +0200

    hasAll, hasAny, fromHash to stx.Hashes

--- src/main/haxe/stx/Hashes.hx
+++ src/main/haxe/stx/Hashes.hx
@@ -4,7 +4,9 @@ package stx;
  * ...
  * @author 0b1kn00b
  */
-import stx.Tuples;
+import stx.Tuples;  		using stx.Tuples;
+												using stx.Iterators;
+using Stax;
 
 class Hashes {
 
@@ -17,11 +19,36 @@ class Hashes {
 	}*/
 	public static function fromHash<A>(h:Hash<A>):Iterable<Tuple2<String,A>>{
 		return
-			h.toIterable().keys().map(
-				function(x){
+			h.keys().toIterable().map(
+				function(x:String){
+					var val : A = h.get(x); 
 					return
-						x.entuple( h.get(x) );
+						x.entuple( val );
 				}
 			);
+	}
+	public static inline function hasAll(h:Hash<Dynamic>, entries:Array<String>):Bool {
+		var ok = true;
+		
+		for (val in entries) {
+			if ( !h.exists(val) ) {
+				ok = false;
+				break;
+			}
+		}
+		return ok;
+	}
+	public static inline function hasAny(h:Hash<Dynamic>, entries:Array<String>):Bool {
+		for (val in entries) {
+			if ( h.exists(val) ) {
+				return true;
+			}
+		}
+		return false;		
+	}
+}
+class HashType{
+	static public function exists<A>(h:Hash<A>,str:String){
+		return h.exists(str);
 	}
 }
