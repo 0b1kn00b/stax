8c34c46a8073e643013018b763cf7fabd662e225
0b1Kn00b <polysemantic@gmail.com>
2012-07-03 21:41:07 +0200

    Implementation of State monad added

---
+++ src/test/haxe/stx/StateTest.hx
@@ -0,0 +1,36 @@
+package stx;
+
+import stx.test.TestCase;
+import stx.test.Assert;
+
+													using stx.States;
+													using stx.Functions;
+													using Std;
+
+class StateTest extends TestCase{
+	public function new(){
+		super();
+	}
+	public function test(){
+		var f 	= 'blah';
+		var sr 	= f.newVar();
+		$type( sr );
+		var fn = function(x) return x;
+		var sr2 = sr.map( fn );
+		$type(fn);
+
+		var fn2 = function(x:String):String { return '$x hmm'.format(); }.modifier;
+		var fn3 = function(x:String):String { return '$x rooga'.format(); }.modifier;
+
+		$type( fn2 );
+		//sr.apply(null).andThen( fn2 );
+		//trace( untyped sr(null)._1 );	}
+		var sr3 = new stx.States.StateRef('pooty');
+		var val = sr.flatMap( fn2 ).flatMap( fn3 );
+		$type (val.map) ;
+		trace(val(null)._1);
+		var val2 = sr.flatMap( StateRefs.reader );
+		trace(val2(null));
+		trace('___________');
+	}
+}
