31a9fd48b446c8cfd0c3520c026a117d367e0c1e
0b1Kn00b <polysemantic@gmail.com>
2012-07-18 14:44:56 +0200

    Some Strings documentation

--- src/main/haxe/stx/Strings.hx
+++ src/main/haxe/stx/Strings.hx
@@ -13,6 +13,9 @@ class Strings {
   static var SepAlphaPattern        = ~/(-|_)([a-z])/g;
   static var AlphaUpperAlphaPattern = ~/-([a-z])([A-Z])/g;
 
+  /**
+  * Returns true if v is 'true' or '1', false if 'false' or '0' and d otherwise.
+  */
   static public function toBool(v: String, ?d: Bool): Bool {
     if (v == null) return d;
     
@@ -20,61 +23,103 @@ class Strings {
     
     return (if (vLower == 'false' || v == '0') Some(false) else if (vLower == 'true' || v == '1') Some(true) else None).getOrElseC(d);
   }
+  /**
+  * Returns an Int from String format, defaulting to d
+  */
   static public function int(v: String, ?d: Null<Int>): Int {
     if (v == null) return d;
     
     return Std.parseInt(v).toOption().filter(function(i) return !Math.isNaN(i)).getOrElseC(d);
   }
+  /**
+  * Returns a Float from String format, defaulting to d
+  */
   static public function toFloat(v: String, ?d: Null<Float>): Float { 
     if (v == null) return d;
     
     return Std.parseFloat(v).toOption().filter(function(i) return !Math.isNaN(i)).getOrElseC(d);
   }
+  /**
+  * Returns true if frag is at the beginning of String v, false otherwise.
+  */
   static public function startsWith(v: String, frag: String): Bool {
     return if (v.length >= frag.length && frag == v.substr(0, frag.length)) true else false;
   }
+  /**
+  * Returns truw if frag is at the end of String v, false otherwise
+  */
   static public function endsWith(v: String, frag: String): Bool {
     return if (v.length >= frag.length && frag == v.substr(v.length - frag.length)) true else false;
   }
+  /*
+  * Returns v as a url encoded String.
+  */
   static public function urlEncode(v: String): String {
     return StringTools.urlEncode(v);
   }
+  /*
+  * Decodes a url encoded String.
+  */
   static public function urlDecode(v: String): String {
     return StringTools.urlDecode(v);
-  }  
+  } 
+  /*
+  * Escapes an html encoded String.
+  */
   static public function htmlEscape(v: String): String {
     return StringTools.htmlEscape(v);
   }
+  /*
+  * Unescapes an html encoded String.
+  */
   static public function htmlUnescape(v: String): String {
     return StringTools.htmlUnescape(v);
   }
+  /*
+  * Removes spaces at the left of the String s.
+  */
   static public function trim(v: String): String {
     return StringTools.trim(v);
   }
+  /**
+  * Returns true if v contains s, false otherwise.
+  */
   static public function contains(v: String, s: String): Bool {
     return v.indexOf(s) != -1;
   }
+  /*
+  * Returns a String where sub is replaced by by in s
+  */
   static public function replace( s : String, sub : String, by : String ) : String {
     return StringTools.replace(s, sub, by);
-  }    
+  }
+  /*
+  * Returns 0 if v1 equals v2, 1 if v1 is bigger than v2, or .1 if v1 is smaller than v2
+  */
   static public function compare(v1: String, v2: String) { 
   return (v1 == v2) ? 0 : (v1 > v2 ? 1 : -1);
   }
+  /**
+  * Returns true if v1 equals v2, flase otherwise.
+  */
   static public function equals(v1: String, v2: String) {
     return v1 == v2;
   }
+  /**
+  * Identity function.
+  */
   static public function toString(v: String): String {
     return v;
   }
-	static public function surround(str:String,before:String,after:String){
-		return before + str + after;
-	}
-	static public function prepend(str:String,before:String){
-		return before + str;
-	}
-	static public function append(str:String,after:String){
-		return str + after;
-	}
+  static public function surround(str:String,before:String,after:String){
+    return before + str + after;
+  }
+  static public function prepend(str:String,before:String){
+    return before + str;
+  }
+  static public function append(str:String,after:String){
+    return str + after;
+  }
   static public function cca(str:String,i:Int){
     return str.charCodeAt(i);
   }
