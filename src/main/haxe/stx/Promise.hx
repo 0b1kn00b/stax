0e2d4a5cad124e3de7775e789e60160f9c5ec79b
0b1Kn00b <polysemantic@gmail.com>
2012-07-18 09:39:44 +0200

    documentation for Promise

--- src/main/haxe/stx/Promise.hx
+++ src/main/haxe/stx/Promise.hx
@@ -19,17 +19,32 @@ import stx.error.NullReferenceError;
 class Promise<A,B>  {
   public static var count : Int = 0;
   
+  /**
+  * @private 
+  * The underlying Future wrapped by this Promise.
+  */
   var fut:Future<Either<A,B>>;
-  public var userCancel:Arrow<A,A>;
+
+  /**
+  * @private
+  * An error handler that flows through th monad chain
+  */
+  public var userCancel(default,null) :Arrow<A,A>;
+
+  /**
+  * @private
+  * Has this Promise been resolved?
+  */
   private var done : Bool;
   
-  public var id : Int;
-  
+  /**
+  * @private 
+  * A reference to the error val if this resolves left
+  */
   var err: Option<A>;
   
   public function new(?cancel:Arrow < A, A > ) {    
     done    = false;
-    id      = count++;
     //trace(this + 'new');
     fut     = new Future();
     err     = None;
@@ -38,49 +53,54 @@ class Promise<A,B>  {
     }
   }
   public function toString() {
-    return 'Promise: [' + id + '] ';
+    return 'Promise';
   }
   public function isDone() {
     return fut.isDone() && this.done;
   }
+  /**
+  * @private
+  * Called internally on failure.
+  */
   function onCancel(e:A):Void {
-    //trace(this + ' cancel: ' + e + ' already done? ' + isDone());
     if (isDone()) return;
       err = Some(e);
       if (userCancel != null) {
         userCancel.run(e);
-        //trace(this + 'running canceller');
-      }else {
-        //trace(this + ' no canceller');
-      }     
+      }
       done = true;
   }
+  /**
+  * Calls a method f with a value when the promise is a success.
+  */
   public function foreach(f): Promise<A,B> {
     return deliverTo(f);
   }
+  /**
+  * Returns the future that this object wraps
+  */
   public function future():Future<Either<A,B>> {
     return fut;
   }
+  /**
+  * Add an error handler to be called if the Promise is broken.
+  */
   public function onError(cb:Arrow<A,A>) : Promise < A, B > {
     if (cb == null) { throw new stx.error.NullReferenceError('cb'); }
     
-    //trace(this + ' onError set ' + userCancel + ' ' + cb + 'with error ' + err);
     if (userCancel == null) {
       userCancel = cb;
     }else {
-      //userCancel = cb;
       userCancel = userCancel.then(cb);
     }
     switch (err) {
       case Some(v):
-        //trace(this + ' calling on set error handler');
         switch (err) {
           case Some(v):
             err = Some(v);
             if (userCancel != null) {
                 userCancel.run(v);
-                //trace(this + 'running canceller');
-              }
+            }
           default:
             
         }
@@ -89,27 +109,32 @@ class Promise<A,B>  {
     }
     return cast this;
   }
+  /**
+  * Retrieves the error if the Promise has been broken.
+  */
   public function error() : Option<A> {
     return err;
   }
+  /**
+  * Calls a method f with a value when the promise is a success.
+  */
   public function deliverTo(cb:B->Void): Promise<A,B> {
-    
     fut.deliverTo(function(e) {
-      //trace(this + 'deliver future');
         switch(e) {
         case Right(v):
           cb(v);
-          //trace(this + 'success delivered');
         case Left(v):
-          //trace(this + 'fail delivered');
           onCancel(v);
         }
       });
     return cast this;
   }
-  
+  /**
+  * Resolve this promise:
+  * Either.Left(value) indicates a failure
+  * Either.Right(value) indicates a success
+  */
   public function resolve(e:Either < A, B > , ?pos ) {
-    //trace(this + 'resolve: ' + e);
     fut.deliver(e, pos);
     switch (e) {
       case Left(v):
@@ -117,48 +142,52 @@ class Promise<A,B>  {
             onCancel(v);
           }
       default:
-        
     }
   }
+  /**
+  * Notify this Promise that the operation has failed.
+  */
   public function left(a:A):Promise<A,B> {
     resolve(Left(a));
     return cast this;
   }
+  /**
+  * Notify this Promise that the operation has been successful.
+  */
   public function right(b:B):Promise<A,B> {
     resolve(Right(b));
     return cast this;
   }
+  /**
+  * Creates a Promise that delivers the value of the orignal 
+  * Promise modified by function 'f'
+  */
   public function map<S>(f: B -> S): Promise < A, S > {
-    //trace(this + 'map');
     var nf = new Promise();
     var uc = userCancel;
     nf.err = err;
     
     this.onError(
         function(x) {
-          //trace( this + 'next future fail');
           nf.onCancel(x);
           return x;
         }.lift()
     );
     fut.deliverTo(function(e) {
-      //trace(this + 'calling map');
         switch(e) {
         case Right(t):
-          //trace(this + 'map ok ' + e);
           nf.right(f(t));
         case Left(msg):
-          //trace(this + 'map fail ' + msg);
           onCancel(msg);
-          //nf.onCancel(msg);
-          //nf.cancel();
-          //nf.left(msg);//cancel();
         }
       });
     return nf;
   }
-  public function flatMap<S>(cb:B->Promise < A, S > ):Promise < A, S > {    
-    //trace(this + 'flatmap');
+  /**
+  * Creates a Promise that first waits for this Promise to resolve, 
+  * and then waits for the Promise return by f, and delivers the result of the second.
+  */
+  public function flatMap<S>(f:B->Promise < A, S > ):Promise < A, S > {    
     var nf = new Promise();
     nf.err = err;
     this.onError(
@@ -168,56 +197,48 @@ class Promise<A,B>  {
         }.lift()
     );
     fut.deliverTo(function(either) {
-      //trace(this + 'calling flatmap: ' + either);
         switch(either) {
         case Right(result):
-            var op = cb(result);
-                //trace( 'flatmap result is ' + op);
+            var op = f(result);
                 op.onError(
                     function(x) {
-                      //trace(this + 'flatmapped fail: ' + nf);
                       nf.onCancel(x);
                       return x;
                     }.lift()
                 );
-    /*            this.onError( 
-                    function(x) {
-                      //trace(this + 'this fail in flatmap');
-                      nf.left(x);
-                      return x;
-                    }.lift()
-                );*/
             op.deliverTo(function(r) {
-              //trace(this + 'flatmap ok ' + err);
               nf.resolve(Right(r));
             });
         case Left(msg):
-            //trace(this + 'flatmap fail ' + err);
-            /*onCancel(msg);
-            nf.onCancel(msg);*/
-            //nf.left(msg);
-            //nf.cancel();
-            //nf.onCancel(msg);//nf.cancel();
         }
       });
     return nf;
   }
-
+  /**
+  * Cancels this Promise
+  */
   public function cancel() {
     fut.cancel();
   }
-
+  /**
+  * Creates a successful Promise of value
+  */
   public static function success<A>(value:A):Promise < Dynamic, A > {
     var o = new Promise();
         o.right(value);
     return o;
   }
-  
+  /**
+  * Creates a broken promise of value.
+  */
   public static function failure<A>(value:A):  Promise<A,Dynamic>{
     var o = new Promise();
         o.left(value);
     return o;
   }
+  /**
+  * Creates a callback (err,val) that will deliver to promise
+  */
   public static function fromCallback<T>(promise:Promise<Dynamic,T>):Dynamic -> Dynamic -> Void {
     return function(err, val) {
       if (err != null) {
@@ -227,35 +248,37 @@ class Promise<A,B>  {
       }
     }
   }
+  /**
+  * Pipes the output of this promise to callback cb
+  */
   public function toCallback(cb:A -> B -> Void) {
     if (cb == null) {
       throw new NullReferenceError('cb');
     }
     this.deliverTo( 
         function(b) {
-          //trace(this + 'cb');
           cb(null, b);
         }
     );
-    //trace(this + 'callback set');
-    onError(function(x) { //trace(this + 'cb error');
+    onError(function(x) { 
       cb(x, null); return x; }.lift()  );
     return cast this;
   }
+  /**
+  * Shortcut for parallel, untyped execution of an Arrays Promises
+  */
   public static function
   waitFor(toJoin:Array<Promise<Dynamic,Dynamic>>):Promise<String,Array<Dynamic>> {
-    //trace('wait for');
     var f0 = false;
     var
       oc = new Promise(),
       results = [];
       toJoin.foreach(
         function(x) {
-          //trace('wait failer setup');
           x.onError(
             function(x) {
               if (oc.userCancel != null && !f0) {
-                oc.userCancel.run(x);
+                //oc.userCancel.run(x);
                 f0 = true;
               }
               return x;
@@ -269,7 +292,6 @@ class Promise<A,B>  {
           if(!failed){
             if (el.isLeft()) {
               failed = true;
-              //trace(oc + 'wait fail ' + el.left().get());
               oc.resolve(Left(el.left().get()));
               return;
             }
@@ -277,7 +299,6 @@ class Promise<A,B>  {
           }
         });
         if (!failed ) {
-          //trace(oc + 'wait succeed' );
           oc.resolve(Right(results));
         }
       });
