cd4dfa9bf9d07fb057b33e29912237d54f5ca20e
0b1Kn00b <polysemantic@gmail.com>
2012-09-19 12:17:33 +0200

    stx.error.Error moved to stx.Error

---
+++ src/main/haxe/stx/Error.hx
@@ -0,0 +1,45 @@
+package stx;
+
+import haxe.Stack;
+import haxe.PosInfos;
+
+class Error {
+
+	public static var exception(get_exception, null):Future<Error>;
+	private static function get_exception() {
+		if (exception == null) { exception = new Future(); }
+		return exception;
+	}
+	
+	var msg : String;
+	var pos	: PosInfos;
+	
+	public function new(msg:String,?pos:PosInfos) {
+		this.msg = msg;
+		this.pos = pos;
+	}
+	public function except(){
+		exception.deliver( this );
+		return this;
+	}
+	public function toString():String{
+		return "Error: (" + this.msg + " at " + Positions.toString(pos) + ")";
+	}
+	@:noUsing
+	public static function create(msg:String,?pos:PosInfos){
+		return new Error(msg, pos);
+	}
+	static public function toError(msg:String,?pos:PosInfos){
+		return Error.create(msg,pos);
+	}
+}
+class Positions {
+	public static function toString(pos:PosInfos){
+		if (pos == null) return 'nil';
+		var type                = pos.className.split(".");
+		return type[type.length-1] + "::" + pos.methodName + "#" + pos.lineNumber;
+	}
+	public static function here(?pos:PosInfos) {
+		return pos;
+	}
+}
