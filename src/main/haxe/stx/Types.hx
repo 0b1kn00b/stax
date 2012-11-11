9a1697934467cc53d82fbd653f0469abd0dd42b8
0b1Kn00b <polysemantic@gmail.com>
2012-11-11 07:02:11 +0100

    Formatting

--- src/main/haxe/stx/Types.hx
+++ src/main/haxe/stx/Types.hx
@@ -1,4 +1,9 @@
 package stx;
+
+using stx.Prelude;
+using stx.Arrays;
+
+import stx.Tuples;
 import stx.Prelude;
 
 using stx.Options;
@@ -6,34 +11,49 @@ using stx.Functions;
 using stx.Error;
 
 class Types{
-	/**
-		Safe resolveClass
-	*/
-	static public function resolveClass(s:String):Option<Class<Dynamic>>{
-		return Options.create(Type.resolveClass(s));
-	}
-	static public function getSuperClass(type:Class<Dynamic>):Option<Class<Dynamic>>{
-		return Options.create(Type.getSuperClass(type));
-	}
-	static public function createInstance<A>(type:Class<A>,args:Array<Dynamic>):Either<Error,A>{
-		return 
-			Type.createInstance.lazy(type,args).catching();
-	}
+  /**
+    Safe resolveClass
+  */
+  static public function resolveClass(s:String):Option<Class<Dynamic>>{
+    return Options.create(Type.resolveClass(s));
+  }
+  static public function getSuperClass(type:Class<Dynamic>):Option<Class<Dynamic>>{
+    return Options.create(Type.getSuperClass(type));
+  }
+  static public function createInstance<A>(type:Class<A>,args:Array<Dynamic>):Either<Error,A>{
+    return 
+      Type.createInstance.lazy(type,args).catching()();
+  }
 
-	/**
-	  Does ´type´ exist in the Class hierarchy?
-	  @param	type
-	  @param	sup
-	*/
-	@:thx
-	public static function hasSuperClass(type : Class<Dynamic>, sup : Class<Dynamic>):Bool
-	{
-		while(null != type)
-		{
-			if(type == sup)
-				return true;
-			type = Type.getSuperClass(type);
-		}
-		return false;
-	}
+  /**
+    Does ´type´ exist in the Class hierarchy?
+    @param  type
+    @param  sup
+  */
+  @:thx
+  public static function hasSuperClass(type : Class<Dynamic>, sup : Class<Dynamic>):Bool
+  {
+    while(null != type)
+    {
+      if(type == sup)
+        return true;
+      type = Type.getSuperClass(type);
+    }
+    return false;
+  }
+  /**
+    @param type      A Type to cast to.
+    @param value     A value to cast.
+    @return          The casted value.
+   */
+   @:thx
+   static public inline function of<T>(type : Class<T>, value : Dynamic) : Null<T>{
+      return (Std.is(value, type) ? cast value : null);
+   }
+
+   static public function extractAllAny<A>(v:A):Array<Tuple2<String,Dynamic>>{
+      var flds = Type.getInstanceFields(Type.getClass(v));
+      return 
+        flds.zip(flds.map(Reflect.field.p1(v)));
+   }
 }
