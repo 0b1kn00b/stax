06b64c987259faa449f83153fedb5ca66e583def
0b1Kn00b <polysemantic@gmail.com>
2012-11-11 06:39:08 +0100

    Can safetly duplicate Arrow primitives for Functions without aliasing

--- src/main/haxe/stx/ArrowFn.hx
+++ src/main/haxe/stx/ArrowFn.hx
@@ -1,5 +1,6 @@
 package stx;
 
+using stx.ArrowFn;
 using stx.Tuples;
 using stx.Prelude;
 using stx.Functions;
@@ -11,13 +12,13 @@ class ArrowFn{
   /**
     Returns a function that applies fn1 then fn2 on the input
   */
-  static public function thenFn<A,B,C>(fn1:A->B,fn2:B->C):A->C{
+  static public function then<A,B,C>(fn1:A->B,fn2:B->C):A->C{
     return fn1.andThen(fn2);
   }
   /**
     Returns a function that applies fn1 to the left hand side of a Tuple
   */
-  static public function firstFn<A,B,C,D>(fn1:A->C):Tuple2<A,B>->Tuple2<C,B>{
+  static public function first<A,B,C,D>(fn1:A->C):Tuple2<A,B>->Tuple2<C,B>{
     return 
       function(t:Tuple2<A,B>){
         return Tuples.t2(fn1(t._1),t._2);
@@ -26,7 +27,7 @@ class ArrowFn{
   /**
     Returns a function that applies fn1 to the right hand side of a Tuple
   */
-  static public function secondFn<A,B,C,D>(fn1:B->D):Tuple2<A,B>->Tuple2<A,D>{
+  static public function second<A,B,C,D>(fn1:B->D):Tuple2<A,B>->Tuple2<A,D>{
     return 
       function(t:Tuple2<A,B>){
         return Tuples.t2(t._1,fn1(t._2));
@@ -35,7 +36,7 @@ class ArrowFn{
   /**
     Returns a function that applies fn1 to the left hand side of a Tuple and fn2 to the right.
   */
-  static public function composeFn<A,B,C,D>(fn1:A->B,fn2:C->D):Tuple2<A,C>->Tuple2<B,D>{
+  static public function compose<A,B,C,D>(fn1:A->B,fn2:C->D):Tuple2<A,C>->Tuple2<B,D>{
     return 
       function(t:Tuple2<A,C>):Tuple2<B,D>{
         return Tuples.t2(fn1(t._1),fn2(t._2));
@@ -44,7 +45,7 @@ class ArrowFn{
   /**
     Returns a function that applies a function on the lhs of a tuple to the value on the rhs.
   */
-  static public function applyFn<A,I,O>(fn:A->Tuple2<I->O,I>):Dynamic->O{
+  static public function applyFn<A,I,O>(fn:A->Tuple2<I->O,I>):A->O{
     return 
       function(v:A):O{
         var t = fn(v);
@@ -54,7 +55,7 @@ class ArrowFn{
   /**
     Returns a function that applies a function to the Left value of an Either.
   */
-  static public function leftFn<A,B,C>(fn:A->C):Either<A,B>->Either<C,B>{
+  static public function left<A,B,C>(fn:A->C):Either<A,B>->Either<C,B>{
     return 
       function(e:Either<A,B>):Either<C,B>{
         return 
@@ -67,7 +68,7 @@ class ArrowFn{
   /**
     Returns a function that applies a function to the Right value of an Either.
   */
-  static public function rightFn<A,B,D>(fn:B->D):Either<A,B>->Either<A,D>{
+  static public function right<A,B,D>(fn:B->D):Either<A,B>->Either<A,D>{
     return 
       function(e:Either<A,B>):Either<A,D>{
         return 
@@ -77,7 +78,22 @@ class ArrowFn{
           }
       }
   }
-  static public function identityFn<A,B>():A->B{
-    return cast Prelude.identity();
+  @:noUsing
+  static public function pure<A,B>():A->B{
+    return identityFn();
+  }
+  /**
+    Returns a function that produces a `Tuple2` from a value.
+  */
+  static public function fan<I,O>(a:I->O):I->Tuple2<O,O>{
+    return 
+      a.thenFn(
+        function(x){
+          return Tuples.t2(x,x);
+        }
+      );
+  }
+  static public function constant<A,B>(v:B):A->B{
+    return function(x:A){ return v; }
   }
 }
