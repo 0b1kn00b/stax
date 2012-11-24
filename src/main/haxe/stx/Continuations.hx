d23cb5f6cb03aa70f803faffee0045887d225e87
0b1Kn00b <polysemantic@gmail.com>
2012-11-24 16:14:13 +0100

    Cont rename Continuations, added callcc, pure.

---
+++ src/main/haxe/stx/Continuations.hx
@@ -0,0 +1,63 @@
+package stx;
+
+import stx.Future;
+import stx.Prelude;
+import stx.Tuples;
+
+//from monax
+
+typedef Cont = Continuations;
+class Continuations{
+  @:noUsing
+  static public function create<R,A>(g:RC<R,A>):RC<R,A>{
+    return function(f:A->R) { return g(f); }
+  }
+  @:noUsing
+  static public function pure<A>(a:A):RC<A,A>{
+    return 
+      function(f:A->A){ return f(a); }
+  }
+  static public function apply<R,A>(cont:RC<R,A>,fn:A->R):R{
+    return cont(fn);
+  }
+  static public function flatMap<A,B,R>(m:RC<R,A>,k:A -> RC<R,B>):RC<R,B>{
+    return 
+      function(cont : B -> R){
+        return m(function(a){ return k(a)(cont); });
+      }
+  }
+  static public function map<A,B,R>(m:RC<R,A>, k : A -> B):RC<R,B>{
+    return 
+      function(cont: B->R) {
+        return m(
+          function(a) {
+            return cont(k(a));
+          }
+        );
+      }
+  }
+
+  static public function cc<R,A,B>(f:(A->RC<R,B>)->RC<R,A>):RC<R,A>{
+    return 
+      function(k){
+        return 
+          f(
+            function(a){
+            return 
+              function(x){
+                return k(a);
+              }
+          }
+          )(k);
+      };
+  }
+  static inline public var callcc = cc;
+  
+  static public function toFuture<A>(cont:Receive<A>):Future<A>{
+    var ft = Future.create();
+    cont(
+      function(x) ft.deliver(x)
+    );
+    return ft;
+  }
+}
