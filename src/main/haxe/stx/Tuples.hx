dc380306965501398ed90eedab5b5af4ab06d8c7
0b1Kn00b <polysemantic@gmail.com>
2012-12-04 14:05:16 +0100

    Tuples updated, can now map, better (I think) typing

--- src/main/haxe/stx/Tuples.hx
+++ src/main/haxe/stx/Tuples.hx
@@ -123,6 +123,9 @@ class Tuple2<A, B> extends AbstractProduct {
         return into(t,f);
       }
   }
+  static public function map<A,B>(tpl:Tuple2<A,A>,fn:A->B):Tuple2<B,B>{
+    return fromArray( tpl.elements().map(fn) );
+  }
 	static public function into<A,B,C>(t:Tuple2<A,B>, f : A -> B -> C ) : C{
     return f(t._1, t._2);
   }
@@ -180,8 +183,11 @@ class Tuple3<A, B, C> extends AbstractProduct {
   static public function thd<A, B, C>(t : Tuple3<A, B, C>) return t._3
 
   @:noUsing
-  static public function fromArray(a:Array<Dynamic>){
+  static public function fromArray<A>(a:Array<A>):Tuple3<A,A,A>{
     return new Tuple3(a[0],a[1],a[2]);
+  }
+  static public function map<A,B>(tpl:Tuple3<A,A,A>,fn:A->B):Tuple3<B,B,B>{
+    return fromArray( tpl.elements().map(fn) );
   }
   static public function translate<A,B,C,D,E,F>(t:Tuple3<A,B,C>,f1: A -> D, f2: B -> E, f3 : C -> F):Tuple3<D,E,F>{
     return Tuples.t3(f1(t._1),f2(t._2),f3(t._3));
@@ -228,7 +234,7 @@ class Tuple4< A, B, C, D> extends AbstractProduct {
   static public function frt<A, B, C, D>(t : Tuple4<A, B, C, D>) return t._4
 
 	@:noUsing
-  static public function fromArray(a:Array<Dynamic>){
+  static public function fromArray<A>(a:Array<A>):Tuple4<A,A,A,A>{
     return new Tuple4(a[0],a[1],a[2],a[3]);
   }
 	static public function into<A,B,C,D,E>(t:Tuple4<A,B,C,D>,f : A -> B -> C -> D -> E) : E
@@ -240,7 +246,9 @@ class Tuple4< A, B, C, D> extends AbstractProduct {
         return into(t,f);
       }
   }
-
+  static public function map<A,B>(tpl:Tuple4<A,A,A,A>,fn:A->B):Tuple4<B,B,B,B>{
+    return fromArray( tpl.elements().map(fn) );
+  }
 	static public function first<A, B, C, D>(t : stx.Tuple4<A, B, C, D>) return t._1
 	static public function second<A, B, C, D>(t : stx.Tuple4<A, B, C, D>) return t._2
 	static public function third<A, B, C, D>(t : stx.Tuple4<A, B, C, D>) return t._3
@@ -292,6 +300,9 @@ class Tuple5< A, B, C, D, E> extends AbstractProduct {
   @:noUsing
   static public function fromArray(a:Array<Dynamic>){
     return new Tuple5(a[0],a[1],a[2],a[3],a[4]);
+  }
+  static public function map<A,B>(tpl:Tuple5<A,A,A,A,A>,fn:A->B):Tuple5<B,B,B,B,B>{
+    return fromArray( tpl.elements().map(fn) );
   }
   static public function spread<A,B,C,D,E,F>(f : A -> B -> C -> D -> E -> F){
     return 
