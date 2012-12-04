073dc3d05554aa2be1443c853fa07556625fe278
0b1Kn00b <polysemantic@gmail.com>
2012-12-04 14:05:42 +0100

    Added convenience to ErrorStack

--- src/main/haxe/stx/error/ErrorStack.hx
+++ src/main/haxe/stx/error/ErrorStack.hx
@@ -9,6 +9,13 @@ class ErrorStack<T> extends DataError<Iterable<T>>{
 	static public function create<A>(data:Iterable<A>, ?msg:String, ?pos:PosInfos){
 		return new ErrorStack(data,msg,pos);
 	}
+	@noUsing
+	static public function build<A>(?msg:String,?pos:PosInfos){
+		return 
+			function(iterable:Iterable<A>){
+				return create(iterable,msg,pos);
+			}
+	}
 	public function new(data:Iterable<T>,msg:String = 'Stack of Errors: ',?pos:PosInfos){
 		super(data,'$msg: $data'.format(),pos);
 	}
