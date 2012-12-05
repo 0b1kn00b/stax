dbc0f2426b9041715a41eb77dfdcedd9fc5123a4
0b1Kn00b <polysemantic@gmail.com>
2012-12-05 19:07:35 +0100

    LList

--- src/main/haxe/stx/ds/LList.hx
+++ src/main/haxe/stx/ds/LList.hx
@@ -2,13 +2,13 @@ package stx.ds;
 
 enum LList<E> {
 	Cons(e : E, t : LList<E>);
-	Nil();
+	Nil;
 }
 class LLists{
 	static public function create<A>():LList<A>{
-		return LList.Nil();
+		return LList.Nil;
 	}
 	static public function cons<A>(l:LList<A>,v:A):LList<A>{
-		return LList.Cons(v,a);
+		return LList.Cons(v,l);
 	}
 }
