a5d03851f82fa8a772763e10e3c98afbb8deb6ae
0b1Kn00b <polysemantic@gmail.com>
2012-12-28 19:02:06 +0100

    States now an Apply, typing breakdown

--- src/main/haxe/stx/States.hx
+++ src/main/haxe/stx/States.hx
@@ -15,7 +15,7 @@ class State<S,R> extends Apply<S,Tuple2<R,S>>{
     return new State(opts);
   }
   @:noUsing
-  static public function unit<S,A>(value:A):State<S,A>{
+  static public function toState<S,A>(value:A):State<S,A>{
     return
       State.pure(
         function(s:S):Tuple2<A,S>{
@@ -37,20 +37,26 @@ class State<S,R> extends Apply<S,Tuple2<R,S>>{
     super(opts);
   }
   /**
-  * Run the State with s, throwing away the result and returning just s.
+  * Run `State` with `s`, dropping the result and returning `s`.
   */
   public function exec(s:S):S{
     return apply(s).second();
   }
   /**
-  * Run the State with s, returning the result.
+  * Run `State` with `s`, returning the result.
   */
   public function eval(s:S):R{
     return apply(s).first();
   }
   public function map<R1>(fn:R->R1):State<S,R1>{
     var fn2 : Tuple2<R,S> -> Tuple2<R1,S> = fn.first();
-    return State.pure( this.apply.then( fn2 ) );
+    return 
+      State.pure(
+        function(s:S){
+          var o = this.apply(s);
+          return fn(o._1).entuple(o._2);
+        }
+      );
   }
   public function mapSt(fn:S->S):State<S,R>{
     return
@@ -65,7 +71,7 @@ class State<S,R> extends Apply<S,Tuple2<R,S>>{
     return 
       State.pure(
         apply
-        .then( Tuple2.translate.p3( Compose.pure() ).p2( fn ) )
+        .then( fn.pair(Compose.unit()) )
         .then(
           function(t:Tuple2<State<S,R1>,S>) {
             return t._1.apply(t._2);
@@ -75,7 +81,7 @@ class State<S,R> extends Apply<S,Tuple2<R,S>>{
   }
   public function getSt():State<S,S>{
     return 
-      pure(
+      State.pure(
         function(s:S):Tuple2<S,S>{
           var o = apply(s);
           return Tuples.t2(o._2,o._2);
@@ -84,7 +90,7 @@ class State<S,R> extends Apply<S,Tuple2<R,S>>{
   }
   public function putSt<S,R>(n:S):State<S,R>{
     return 
-      pure(
+      State.pure(
         function (s:S){
           return Tuples.t2(null,n);
         }
@@ -97,7 +103,7 @@ class StateRef<S,A>{
     this.value = v;
   }
   public function read():State<S,A> {
-    return State.unit(value);
+    return State.toState(value);
   }
   public function write(a:A):State<S,StateRef<S,A>>{
     return 
@@ -115,7 +121,7 @@ class StateRef<S,A>{
   }
   
   public static function newVar<S,A>(v:A):State<WorldState,StateRef<WorldState,A>>{
-    return State.unit( new StateRef(v));
+    return State.toState(new StateRef(v));
   }
   public static function run<A>(v:State<WorldState,StateRef<WorldState,A>>){
     v.apply( new WorldState() );
