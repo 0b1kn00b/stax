2d19fc5c44ce760da1c6c5e77df1fc7582fbcae0
0b1kn00b <polysemantic@gmail.com>
2013-10-25 15:24:15 +0200

    stx-Readers now an abstract

--- src/main/haxe/stx/Reader.hx
+++ src/main/haxe/stx/Reader.hx
@@ -2,38 +2,28 @@ package stx;
 import stx.Prelude;
 
 //http://www.youtube.com/watch?v=ZasXwtTRkio
-class Reader<C,A>{
-	@:noUsing
-	static public function create<C,A>( opts: { apply : C -> A } ){
-		return new Reader(opts);
+abstract Reader<C,A>(C->A) from C->A to C->A{
+  public function new(v){
+    this = v;
+  }
+	@:noUsing static public function unit<C,A>():Reader<C,A>{
+		return cast Compose.pure();
 	}
-	@:noUsing
-	static public function unit<A,B>(fn:A->B){
-		return create( { apply :  fn } );
+	@:noUsing static public function pure<C,A>(a:A):Reader<C,A>{
+		return function(c:C){ return a; };
 	}
-	@:noUsing
-	static public function pure<C,A>(a:A):Reader<C,A>{
-		return unit(function(c:C){ return a; });
-	}
-
-	public function new(opts: { apply : C -> A } ){
-		this.apply = opts.apply;
-	}
-	dynamic public function apply(v:C):A{
-		return null;
-	}
-	public function map<B>(f:A->B){
-		return unit( function(i){ return f(apply(i)); } );
-	}
-	public function flatMap<B>(f:A->Reader<C,B>){
-		return unit( function(i){ return f(apply(i)).apply(i); } );
+	public function apply(c:C):A{
+		return this(c);
 	}
 }
 class Readers{
-	static public function map<V,A,B>(reader:Reader<V,A>,f:Function1<A,B>):Reader<V,B>{
-		return reader.map(f);
+	static public function apply<C,A>(rdr:Reader<C,A>):A{
+		return rdr.apply;
 	}
-	static public function flatMap<V,A,B>(reader:Reader<V,A>,f:Function1<A,Reader<V,B>>):Reader<V,B>{
-		return reader.flatMap(f);
+	static public function map<C,A,B>(r:Reader<C,A>,fn:A->B):Reader<C,B>{
+		return function(i:C){ return f(r.apply(i)); }
+	}
+	static public function map<C,A,B>(r:Reader<C,A>,fn:A->B):Reader<C,B>{
+		return function(i:C){ return f(apply(i)).apply(i); }
 	}
 }
